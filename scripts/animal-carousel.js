5459f2290ca380590156f4f3c05a7a1c7504d414
OlehYanochko <yanochko.oleh@gmail.com>
2022-03-28 12:56:49 +0000

    caurosel js added

---
+++ scripts/animal-carousel.js
@@ -0,0 +1,9 @@
+$('.animals').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    swipe: true,
+    swipeToSlide: true,
+    prevArrow: $('.slider-prev'),
+    nextArrow: $('.slider-next'),
+  });
